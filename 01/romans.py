53bf27e01b06e1dbacc27821cfdfd1df09ab3c4a
GabrielDona <gabrieldona2004@gmail.com>
2024-06-18 21:49:38 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,54 +1,46 @@
-number = 1994
-s = "MCMXCIV"
+# Números que terão sua conexão com os símbolos
+num = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
-#Números que terão sua conexão com o simbolos
-num = [1, 4, 5, 9, 10, 40, 50, 90,
-    100, 400, 500, 900, 1000]
+# Símbolos que serão convertidos no lugar dos números
+sym = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
-#Simbolos que serão convertidos no lugar dos números
-sym = ["I", "IV", "V", "IX", "X", "XL",
-    "L", "XC", "C", "CD", "D", "CM", "M"]
-
-#i = ordem em que se encontra os números, sendo o úmero com local 12 o mais alto e local 0 o mais baixo
-i = 12
-
-#definição que cria a lista de números com atribuição de simbolos a cada um deles
 def int_to_roman(number):
-    #cria um enquanto que faz a repetição da ação sempre que etiver nesse processo
-    while number:
-
-        #Operação de divisão do número inputado com número na ordem da lista, sendo a sequencia do mais alto para o mais baixo   
+    i = 0
+    roman_numeral = ""
+    while number > 0:
+        # Operação de divisão do número inputado com número na ordem da lista
         div = number // num[i]
-        #Pega a sobra da divisão e trasnforma no proximo numero a ser dividido, e faz com que volte a acontecer a corda, até acabar os número a serem divididos
+        # Pega a sobra da divisão e transforma no próximo número a ser dividido
         number %= num[i]
-  
-        #Enquanto que faz com que o simbolo do número dividido seja printado e assim passe para o proximo número a ser dividio e printado
-        while div:
-            #Printa o simbolo
-            print(sym[i], end = "")
-            #Pega o numero divido e diminui 1 dele
+        # Enquanto que faz com que o símbolo do número dividido seja adicionado ao resultado
+        while div > 0:
+            roman_numeral += sym[i]
             div -= 1
-        #Diminui em 1 na ordem de sequencia de divisão dos números
-        #Resumindo, passa para o proximo número
-        i -= 1
-        
+        # Passa para o próximo número
+        i += 1
+    return roman_numeral
+sym_to_value = {
+    "M": 1000, "CM": 900, "D": 500, "CD": 400, "C": 100,
+    "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1
+}
+
 def roman_to_int(s):
     i = 0
     total = 0
     n = len(s)
-
     while i < n:
         # Verifica se o par de símbolos forma um numeral especial (por exemplo, "IV", "IX", etc.)
-        if i + 1 < n and s[i:i+2] in sym:
-            total += sym[s[i:i+2]]
+        if i + 1 < n and s[i:i+2] in sym_to_value:
+            total += sym_to_value[s[i:i+2]]
             i += 2
         else:
-            total += sym[s[i]]
+            total += sym_to_value[s[i]]
             i += 1
-            
-      return total
+    return total
 
 if __name__ == "__main__":
-    print("O número romano para o numeral inserido é:", end = " ")
-    printRoman(number)
-    priontInt(s)
+    number = 1994
+    s = "MCMXCIV"
+
+    print("O número romano para o numeral inserido é:", int_to_roman(number))
+    print("O número inteiro para o numeral romano inserido é:", roman_to_int(s))
